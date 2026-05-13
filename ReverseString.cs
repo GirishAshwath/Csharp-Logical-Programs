4df6a4a6494f06f86aa76208030015158adf17fb
GirishAshwath <agirish323@gmail.com>
2026-05-13 07:36:47 -0700

    Update ReverseString.cs

--- ReverseString.cs
+++ ReverseString.cs
@@ -1,2 +1,34 @@
 using System;
-using System.Collection.Generic
+using System.Collection.Generic;
+using System.Text;
+public Class Program
+{
+  public static void Main(string[] args)
+  {
+    string message = "Programming hello";
+
+    StringBuilder reversedWordString = new StringBuilder();
+    List<char> charList = new List<char>();
+
+    for(i =0; i < message.Length; i++)
+    {
+        if(message[i] == ' ' || i== message.length-1)
+        {
+            if(i == message.Length-1)
+               charList.Add(message[i]);
+
+               for(int j = charList.Count-1; j>=0; j--)
+               {
+                 reversedWordString.Append(charList[i]);
+                }
+                reversedWordString.Append(' ');
+                charList = new List<char>();
+        }
+        else
+        {
+          charList.Add(message[i]);
+        }
+    }
+    Console.WriteLine($"Reversed word string is {reversedWordString}");
+  }
+}
